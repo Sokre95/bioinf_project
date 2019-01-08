76845f4b13f0240db700ae658b605bea68a5b08a
Tomislav Bozuric <tomislav.bozuric@fer.hr>
2019-01-08 03:37:22 +0100

    Viterbi optimization and aligner code cleanup

--- Viterbi.cpp
+++ Viterbi.cpp
@@ -22,17 +22,14 @@ std::string Viterbi::alignSequences(Sequence &first, Sequence &second) {
 
     std::vector<char> states;
 
+    double tau = transition_probabilities[_lookupTable.at('t')];
+    double delta = transition_probabilities[_lookupTable.at('d')];
+    double epsilon = transition_probabilities[_lookupTable.at('e')];
+
     for (unsigned long i = 0; i < n; i++) {
         for (unsigned long j = 0; j < m; j++) {
 
-            double tau = transition_probabilities[_lookupTable.at('t')];
-            double delta = transition_probabilities[_lookupTable.at('d')];
-            double epsilon = transition_probabilities[_lookupTable.at('e')];
-
-
             double temp = (1 - epsilon - tau);
-
-
             double max = std::max((1 - 2 * delta - tau) * viterbi_match[i - 1][j - 1],
                                   temp * viterbi_insert_x[i - 1][j - 1]);
 
@@ -41,17 +38,13 @@ std::string Viterbi::alignSequences(Sequence &first, Sequence &second) {
             viterbi_match[i][j] = emission_probabilities[_lookupTable.at(first_sequence.at(i))][_lookupTable.at(
                     second_sequence.at(j))] * max;
 
-
             viterbi_insert_x[i][j] =
                     emission_probabilities[_lookupTable.at(first_sequence.at(i))][_lookupTable.at('-')] *
                     std::max(delta * viterbi_match[i - 1][j], epsilon * viterbi_insert_x[i - 1][j]);
 
-
             viterbi_insert_y[i][j] =
                     emission_probabilities[_lookupTable.at('-')][_lookupTable.at(second_sequence.at(j))] *
                     std::max(delta * viterbi_match[i][j - 1], epsilon * viterbi_insert_y[i][j - 1]);
-
-
         }
     }
     //TODO
--- aligner.py
+++ aligner.py
@@ -1,27 +1,11 @@
 import os
-import sys
 from collections import defaultdict
 
-from Bio.Align.Applications import ClustalwCommandline
-
-CLUSTAL_W_PATH = "/home/tomo/Desktop/clustalw-2.1-linux-x86_64-libcppstatic/clustalw2"
-
-for line in sys.stdin:
-    name_of_file = line.strip()
-    input_file = name_of_file
-    output_file = "../outputs/output_" + name_of_file
-    cline = ClustalwCommandline(CLUSTAL_W_PATH,
-                                infile=input_file,
-                                outfile=output_file, output="fasta", type="DNA")
-    cline()
-
-total_number_of_bases = 0
 thymine = 0
 guanine = 0
 cytosine = 0
 adenine = 0
 
-number_of_chars = 0
 number_of_transitions = 0
 number_of_pairs = 0
 
@@ -29,7 +13,6 @@ pairs_frequency = defaultdict(int)
 transition_frequency = defaultdict(int)
 emission_frequency_x = defaultdict(int)
 emission_frequency_y = defaultdict(int)
-emission_frequency_counter = 0
 states = []
 
 for filename in os.listdir("../outputs"):
@@ -43,37 +26,17 @@ for filename in os.listdir("../outputs"):
         for i in range(len(sequence_a)):
             first_seq = sequence_a[i]
             second_seq = sequence_b[i]
-            number_of_chars += 2  # prva sekvenca jedno slovo i druga jedno
-            if first_seq == 'A':
-                adenine += 1
-            elif first_seq == 'G':
-                guanine += 1
-            elif first_seq == 'C':
-                cytosine += 1
-            elif first_seq == 'T':
-                thymine += 1
 
-            if second_seq == 'A':
-                adenine += 1
-            elif second_seq == 'G':
-                guanine += 1
-            elif second_seq == 'C':
-                cytosine += 1
-            elif second_seq == 'T':
-                thymine += 1
-
+            number_of_pairs += 1
             if first_seq != '-' and second_seq != '-':
                 curr_states.append('M')
-                number_of_pairs += 1
                 pairs_frequency[(first_seq, second_seq)] += 1
             if first_seq != '-' and second_seq == '-':
                 curr_states.append('X')
                 emission_frequency_x[(first_seq, second_seq)] += 1
-                emission_frequency_counter += 1
             if second_seq != '-' and first_seq == '-':
                 curr_states.append('Y')
                 emission_frequency_y[(first_seq, second_seq)] += 1
-                emission_frequency_counter += 1
 
         curr_states.append('E')
         states.append(curr_states)
@@ -82,12 +45,6 @@ for state in states:
     for i in range(len(state) - 1):
         number_of_transitions += 1
         transition_frequency[(state[i], state[i + 1])] += 1
-# 4.0
-#print("Emission probabilities: ")
-#print("p(A) = %f\n", (adenine + 1.0) / (number_of_chars + 2.0))
-#print("p(C) = %f\n", (cytosine + 1.0) / (number_of_chars + 2.0))
-#print("p(G) = %f\n", (guanine + 1.0) / (number_of_chars + 2.0))
-#print("p(T) = %f\n", (thymine + 1.0) / (number_of_chars + 2.0))
 
 print("Pairs probabilities: ")
 for k, v in pairs_frequency.items():  # will become d.items() in py3k, 16.0
@@ -97,10 +54,10 @@ print("Transitions probabilities: ")
 for k, v in transition_frequency.items():  # will become d.items() in py3k, 14
     print("p(%s) = %f\n" % (k, (v + 1.0) / (number_of_transitions + 2.0)))
 
-print("Transitions probabilities: ")
+print("Emission probabilities x: ")
 for k, v in emission_frequency_x.items():  # will become d.items() in py3k, 14
-    print("p(%s) = %f\n" % (k, (v + 1.0) / (emission_frequency_counter + 2.0)))
+    print("p(%s) = %f\n" % (k, (v + 1.0) / (number_of_pairs + 2.0)))
 
-print("Transitions probabilities: ")
+print("Emission probabilities y: ")
 for k, v in emission_frequency_y.items():  # will become d.items() in py3k, 14
-    print("p(%s) = %f\n" % (k, (v + 1.0) / (emission_frequency_counter + 2.0)))
+    print("p(%s) = %f\n" % (k, (v + 1.0) / (number_of_pairs + 2.0)))
