c8643ad879607921ca7ec5bc8797750becbf482e
Martin Pisacic <pisacic.martin2@gmail.com>
2019-01-09 02:22:52 +0100

    Memory leak fix

--- Viterbi.cpp
+++ Viterbi.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include <stdio.h>
 #include <iostream>
 #include "Viterbi.h"
 #include "FastaParser.h"
@@ -27,42 +28,15 @@ float IViterbi::max(float m, float x, float y, byte* result) {
 }
 
 void Viterbi::alignSequences(Sequence *first, Sequence *second) {
-
-    FastaParser parser("../database/pairs/p1.fasta");
-
-    const std::vector<Sequence*> sequences = parser.parse();
-
-    for (int i = 0; i < sequences.size(); ++i) {
-        Sequence *s = sequences.at(i);
-
-        std::cout << s->getDescription() << std::endl;
-
-        for (int j = 0; j < s->getSequence().size(); ++j) {
-            std::cout << s->getSequence().at(j) << std::flush;
-        }
-        std::cout << std::endl << std::endl;
-    }
-
-    const float transmission_prob[3][3] = {
-            { 0.889107, 0.001361, 0.001663 },
-            { 0.001361, 0.000983, 0 },
-            { 0.004233, 0, 0.050042 }
-    };
-
-
-
-
-    std::vector<char> first_sequence = sequences.at(0)->getSequence();
-    std::vector<char> second_sequence = sequences.at(1)->getSequence();
-
-    char val = second_sequence.at(0);
+    std::vector<char> first_sequence = first->getSequence();
+    std::vector<char> second_sequence = second->getSequence();
 
     ulong n = first_sequence.size();
     ulong m = second_sequence.size();
 
-    float viterbi_match[m];
-    float viterbi_insert_x[m];
-    float viterbi_insert_y[m];
+    float* viterbi_match = new float[m];
+    float* viterbi_insert_x = new float[m];
+    float* viterbi_insert_y = new float[m];
 
     memset(viterbi_match, 0, sizeof(float) * m);
     memset(viterbi_insert_x, 0, sizeof(float) * m);
@@ -70,109 +44,95 @@ void Viterbi::alignSequences(Sequence *first, Sequence *second) {
 
     viterbi_match[0] = 1;
 
-    // const float tau = transition_probabilities[_lookupTable.at('t')];
-    // const float delta = transition_probabilities[_lookupTable.at('d')];
-    // const float epsilon = transition_probabilities[_lookupTable.at('e')];
-
     // ovdje zapisujemo prijelaze za sva stanja koja cemo koristiti u backtracku
-    byte transitions_m[n][m];
-    byte transitions_x[n][m];
-    byte transitions_y[n][m];
+    byte* transitions_m = new byte[n * m];
+    byte* transitions_x = new byte[n * m];
+    byte* transitions_y = new byte[n * m];
 
-    // memset(transitions_m, 0, sizeof(char) * n * m);
-    // memset(transitions_x, 0, sizeof(char) * n * m);
-    // memset(transitions_y, 0, sizeof(char) * n * m);
+    memset(transitions_m, 0, sizeof(char) * n * m);
+    memset(transitions_x, 0, sizeof(char) * n * m);
+    memset(transitions_y, 0, sizeof(char) * n * m);
 
     byte previous;
 
     // spremanje medurezultata prethodnog koraka
-    for (ulong i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
+        float* tmp_m = new float[m];
+        float* tmp_x = new float[m];
+        float* tmp_y = new float[m];
 
-        float tmp_m[m];
-        float tmp_x[m];
-        float tmp_y[m];
-
-        // memset(tmp_m, 0, sizeof(float) * m);
-        // memset(tmp_x, 0, sizeof(float) * m);
-        // memset(tmp_y, 0, sizeof(float) * m);
-
-
-        // float* tmp_m = new float[m];
-        // float* tmp_x = new float[m];
-        // float* tmp_y = new float[m];
+        memset(tmp_m, 0, sizeof(float) * m);
+        memset(tmp_x, 0, sizeof(float) * m);
+        memset(tmp_y, 0, sizeof(float) * m);
 
         float v1, v2, v3, max;
 
+        const float v = trans_prob[0][0];
 
-        const float v = transmission_prob[0][0];
+        char &c2 = second_sequence.at(0);
+        char &c1 = first_sequence.at(i);
 
-        char c2 = second_sequence.at(0);
-        char c1 = first_sequence.at(i);
-
-
-        int pos1 = _lookupTable.at(first_sequence.at(i));
-        int pos2 = _lookupTable.at(second_sequence.at(0));
+        int pos1 = _lookupTable.at(c1);
+        int pos2 = _lookupTable.at(c2);
 
         float prob = emission_probabilities[pos1][pos2];
 
         tmp_m[0] = prob * v * viterbi_match[0];  // prijelaz iz M u M
 
-        transitions_m[i][0] = M;
+        *(transitions_m + i * n + 0) = M;
 
-        v1 = transmission_prob[0][1] * viterbi_match[0]; // iz M u X
-        v2 = transmission_prob[1][1] * viterbi_insert_x[0]; // iz X u X
+        v1 = trans_prob[0][1] * viterbi_match[0]; // iz M u X
+        v2 = trans_prob[1][1] * viterbi_insert_x[0]; // iz X u X
         max = this->max(v1, v2, M, X, &previous);
 
         tmp_x[0] = emission_probabilities[_lookupTable.at(first_sequence.at(i))][_lookupTable.at('-')] * max;
-        transitions_x[i][0] = previous;
+        *(transitions_x + i * n + 0) = previous;
 
         tmp_y[0] = 0; // za j = 1 ovo je uvijek nula prema pretpostavci
-        transitions_y[i][0] = M;
+        *(transitions_y + i * n + 0) = M;
 
         for (ulong j = 1; j < m; j++) {
             // moramo vidjet sta cemo dalje zapisati u tmp polja, i sta cemo koristiti za racunanje
 
             // ovo sve uzimamo iz prethodne iteracije zbog (i - 1) u formuli
-            v1 = transmission_prob[0][0] * viterbi_match[j - 1]; // iz M u M
-            v2 = transmission_prob[1][0] * viterbi_insert_x[j - 1]; // iz X u M
-            v3 = transmission_prob[2][0] * viterbi_insert_y[j - 1]; // iz Y u M
+            v1 = trans_prob[0][0] * viterbi_match[j - 1]; // iz M u M
+            v2 = trans_prob[1][0] * viterbi_insert_x[j - 1]; // iz X u M
+            v3 = trans_prob[2][0] * viterbi_insert_y[j - 1]; // iz Y u M
 
             max = this->max(v1, v2, v3, &previous); // pronademo max
 
             tmp_m[j] = emission_probabilities[_lookupTable.at(first_sequence.at(i))][_lookupTable.at(second_sequence.at(j))] * max;
-            transitions_m[i][j] = previous;
+            *(transitions_m + i * n + j) = previous;
 
-            v1 = transmission_prob[0][1] * viterbi_match[j]; // iz M u X
-            v2 = transmission_prob[1][1] * viterbi_insert_x[j]; // iz X u X
+            v1 = trans_prob[0][1] * viterbi_match[j]; // iz M u X
+            v2 = trans_prob[1][1] * viterbi_insert_x[j]; // iz X u X
 
             max = this->max(v1, v2, M, X, &previous);
             tmp_x[j] = emission_probabilities[_lookupTable.at(first_sequence.at(i))][_lookupTable.at('-')] * max;
-            transitions_x[i][j] = previous;
+            *(transitions_x + i * n + j)  = previous;
 
-            v1 = transmission_prob[0][2] * tmp_m[j - 1]; // iz M u Y
-            v2 = transmission_prob[2][2] * tmp_y[j - 1]; // iz Y u Y
+            v1 = trans_prob[0][2] * tmp_m[j - 1]; // iz M u Y
+            v2 = trans_prob[2][2] * tmp_y[j - 1]; // iz Y u Y
             max = this->max(v1, v2, M, Y, &previous);
 
             tmp_y[j] = emission_probabilities[_lookupTable.at('-')][_lookupTable.at(second_sequence.at(j))] * max;
-            transitions_y[i][j] = previous;
+            *(transitions_y + i * n + j)  = previous;
         }
 
-        // izbrisi memoriju koju su zauzela stara polja
-        //delete [] viterbi_match;
-        //delete [] viterbi_insert_x;
-        //delete [] viterbi_insert_y;
+        memcpy(viterbi_match, tmp_m, m);
+        memcpy(viterbi_insert_x, tmp_x, m);
+        memcpy(viterbi_insert_y, tmp_y, m);
 
-        // zamijeni polja
-        //viterbi_match = tmp_m;
-        //viterbi_insert_x = tmp_x;
-        //viterbi_insert_y = tmp_y;
+        delete [] tmp_m;
+        delete [] tmp_x;
+        delete [] tmp_y;
     }
 
     // svako slovo mapira se na prvi element 2D polja
     std::map<byte, byte*> transitions_lookup = {
-            {M, transitions_m[0]},
-            {X, transitions_x[0]},
-            {Y, transitions_y[0]}
+            {M, transitions_m},
+            {X, transitions_x},
+            {Y, transitions_y}
     };
 
     // ovdje cemo zapisivat poravnanja
@@ -193,11 +153,11 @@ void Viterbi::alignSequences(Sequence *first, Sequence *second) {
     ulong j = m - 1;
 
     if (vm > vx && vm > vy) {
-        trans = transitions_m[0];
+        trans = transitions_m;
     } else if (vx > vm && vx > vy) {
-        trans = transitions_x[0];
+        trans = transitions_x;
     } else {
-        trans = transitions_y[0];
+        trans = transitions_y;
     }
 
     // ove dolje korake moramo ponavljat sve dok ne dobijemo ukupnu sekvencu stanja
@@ -227,9 +187,13 @@ void Viterbi::alignSequences(Sequence *first, Sequence *second) {
         trans = transitions_lookup[state]; // iz onoga sta je zapisano u polju odredujemo sljedecu tablicu
     }
 
-    // delete [] viterbi_match;
-    // delete [] viterbi_insert_x;
-    // delete [] viterbi_insert_y;
+    delete transitions_x;
+    delete transitions_y;
+    delete transitions_m;
+
+    delete [] viterbi_match;
+    delete [] viterbi_insert_x;
+    delete [] viterbi_insert_y;
 }
 
 Viterbi::Viterbi(const float (&emission_probabilities)[5][5], const float (&trans_prob)[3][3]): IViterbi(emission_probabilities, trans_prob) {}
